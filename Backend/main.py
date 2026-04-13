0827bcab285b0f3b71e157122e1f3aec73542754
Ciprian D <147750749+kiprinel05@users.noreply.github.com>
2026-04-13 16:19:02 +0300

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -25,6 +25,7 @@ from features.pixy_learns.router import router as pixy_learns_router
 from features.drawing.router import router as drawing_router
 from features.numbers.router import router as numbers_router
 from features.workshop.router import router as workshop_router
+from features.animals.router import router as animals_router
 from features.leaderboard.router import router as leaderboard_router
 
 app = FastAPI(
@@ -96,6 +97,7 @@ app.include_router(pixy_learns_router, prefix=settings.API_V1_STR)
 app.include_router(drawing_router, prefix=settings.API_V1_STR)
 app.include_router(numbers_router, prefix=settings.API_V1_STR)
 app.include_router(workshop_router, prefix=settings.API_V1_STR)
+app.include_router(animals_router, prefix=settings.API_V1_STR)
 app.include_router(leaderboard_router, prefix=settings.API_V1_STR)
 
 @app.get("/")
